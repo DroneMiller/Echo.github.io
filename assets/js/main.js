619266406fba8bb965ad22781b9e7cebb2530568
DroneMiller <168025314+DroneMiller@users.noreply.github.com>
2024-04-24 23:11:39 +0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -112,3 +112,13 @@ tabs.forEach((tab) => {
         tab.classList.add('active-tab');
     });
 });
+/*=============== Tawk.to Script ===============*/
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6629668f1ec1082f04e67593/1hs8rh94f';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
